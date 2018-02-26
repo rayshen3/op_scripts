d4f76810d6860fd60580adf077680cc7f18a477c
rayshen <rayshen3@gmail.com>
2018-02-26 14:25:01 +0800

    Create collectionsize.js

---
+++ collectionsize.js
@@ -0,0 +1,4 @@
+var collectionNames = db.getCollectionNames(), stats = [];
+collectionNames.forEach(function (n) { stats.push(db[n].stats()); });
+stats = stats.sort(function(a, b) { return b['size'] - a['size']; });
+for (var c in stats) { print(stats[c]['ns'] + ": " + stats[c]['size'] + " (" + stats[c]['storageSize'] + ")"); }
