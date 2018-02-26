133abc6515cadd55ab63d4d8602e132eee17f4c9
rayshen <rayshen3@gmail.com>
2018-02-26 14:28:16 +0800

    Update collectionsize.js

--- collectionsize.js
+++ collectionsize.js
@@ -1,3 +1,8 @@
+/*
+ref: https://makandracards.com/luis-romero/30231-mongodb-list-all-collections-by-size
+mongo --port 27018 collection_name test.js
+*/
+
 var collectionNames = db.getCollectionNames(), stats = [];
 collectionNames.forEach(function (n) { stats.push(db[n].stats()); });
 stats = stats.sort(function(a, b) { return b['size'] - a['size']; });
